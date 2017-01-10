5bf14af8a4d2996d725231161a3eea7420194da1
james <dimitriosnikolaidis@hotmail.com>
2017-01-10 19:19:15 +0200

    one comment

--- app/src/main/java/com/applicationforlife/jamesnikolaidis/thesis_farmers_helper/FarmerActivity.java
+++ app/src/main/java/com/applicationforlife/jamesnikolaidis/thesis_farmers_helper/FarmerActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
 
 public class FarmerActivity extends AppCompatActivity {
-
+//one comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
